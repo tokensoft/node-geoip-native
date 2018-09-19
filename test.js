a65b5f6052f015e4f6090110f8745a32046d0449
pooleja <poole.james@gmail.com>
2018-09-19 12:22:38 -0700

    Update country db

--- test.js
+++ test.js
@@ -1,50 +1,43 @@
-var geoip = require("./geoip.js");
-//var geoip = require("geoip-lite");
+var geoip = require('./geoip.js')
+// var geoip = require("geoip-lite");
 
-var test1 = true;
+var test1 = true
 
-function test() {
+function test () {
+  var total = 0
+  var numtests = 20
+  var numiterations = 1000000
 
-    var total = 0;
-    var numtests = 20;
-    var numiterations = 1000000;
+  console.log('starting test: ' + (test1 ? 'geoip-native' : 'geoip-lite'))
 
-    console.log("starting test: " + (test1 ? "geoip-native" : "geoip-lite"));
+  for (var t = 0; t < numtests; t++) {
+    var start = new Date().getTime()
 
-    for(var t=0; t<numtests; t++) {
-
-        var start = new Date().getTime();
-
-        for(var i=0; i<numiterations; i++) {
-
-            var o1 = 1 + Math.round(Math.random() * 254);
-            var o2 = 1 + Math.round(Math.random() * 254);
-            var o3 = 1 + Math.round(Math.random() * 254);
-            var o4 = 1 + Math.round(Math.random() * 254);
-            var ip = o1 + "." + o2 + "." + o3 + "." + o4;
-            geoip.lookup(ip);
-        }
-
-        var finish = new Date().getTime();
-
-        if(t > 4 && t < 15) {
-            total += (finish - start);
-            console.log("time " + (finish - start));
-        }
+    for (var i = 0; i < numiterations; i++) {
+      var o1 = 1 + Math.round(Math.random() * 254)
+      var o2 = 1 + Math.round(Math.random() * 254)
+      var o3 = 1 + Math.round(Math.random() * 254)
+      var o4 = 1 + Math.round(Math.random() * 254)
+      var ip = o1 + '.' + o2 + '.' + o3 + '.' + o4
+      geoip.lookup(ip)
     }
 
-    console.log("average: " + (total / 10));
+    var finish = new Date().getTime()
 
-    if(!test1) {
-        return;
+    if (t > 4 && t < 15) {
+      total += finish - start
+      console.log('time ' + (finish - start))
     }
+  }
 
-    geoip = require("geoip-lite");
-    test1 = false;
-    test();
+  console.log('average: ' + total / 10)
+
+  if (!test1) {
+
+  }
 }
 
-setTimeout(test, 3000);
+setTimeout(test, 3000)
 
 /*
 benchmark results:
@@ -73,4 +66,4 @@ benchmark results:
  time 8303
  time 8416
  time 8261
- average: 8375.3*/
+ average: 8375.3 */
